63a98b93cc5c6ccc41bf2cecfc43b11c12a78cd5
Zeinab <zalhalabi@students.haigazian.edu.lb>
2026-07-02 19:10:49 +0300

    complete angular project

--- class-work/portfolio-project-zeinab/src/app/app.routes.ts
+++ class-work/portfolio-project-zeinab/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: About
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'projects',
+    component: Projects
+  }
+];
--- class-work/portfolio-project-zeinab/src/app/app.ts
+++ class-work/portfolio-project-zeinab/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
-
+import { Profile } from './profile/profile';
+import { About } from './about/about';
+import { Projects } from './projects/projects';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, About, Profile, Projects],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
