aca59ed1bd5a785c34477fc2e4177f40c7f97b83
Zeinab <zalhalabi@students.haigazian.edu.lb>
2026-07-02 15:56:58 +0300

    navbar component

---
+++ class-work/portfolio-project-zeinab/src/app/profile/profile.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.html',
+  styleUrl: './profile.scss',
+})
+export class Profile {}
